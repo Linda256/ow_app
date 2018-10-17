e8d4ddf3f869c577e67f572afa8b1ef6033ee1a3
Linda Wang <lindawang256@gmail.com>
2018-10-17 09:55:24 -0700

    Set up'/' '/ math' route

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ const calculateNum = require('./calculateNum');
 let count=0;
 const app = express();
 const port = 3000;
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
+app.get('/',(req,res) =>{
+  count++;
+  const visits = {visits: count}
+  res.json(visits);
+})
+
+app.post('/math',(req,res) =>{
+  console.log(req.body.input);
+  let expStr = req.body.input;
+  let result=calculateNum(expStr);
+
+  let output =  {result: result};
+  res.json(output);
+})
 
 app.listen(port, ()=>console.log(`Server running on port ${port}`));
