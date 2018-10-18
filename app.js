8ff7c842f911fd39ef5ccd2f256252d927a00638
Linda Wang <lindawang256@gmail.com>
2018-10-17 21:25:38 -0700

    Add /game route to render 3d cube

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+
+
+//load calculateNum.js to do math
 const calculateNum = require('./calculateNum');
 
 let count=0;
 const app = express();
 const port = 3000;
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
@@ -22,6 +26,12 @@ app.post('/math',(req,res) =>{
 
   let output =  {result: result};
   res.json(output);
+})
+
+app.use('/game',express.static('public'));
+
+app.get('/game',(req,res)=>{
+  res.sendFile(__dirname+'/public/index.html');
 })
 
 app.listen(port, ()=>console.log(`Server running on port ${port}`));
